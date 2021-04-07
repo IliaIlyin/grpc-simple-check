4602cbd6ee31fd782de35a8b1e1b1e97e25ab144
illia.ilin <illia.ilin@teamdev.com>
2021-04-07 15:59:21 +0300

    while true added

--- HelloClient.cpp
+++ HelloClient.cpp
@@ -10,26 +10,29 @@ void HelloClient::RunClient()
     auto request = test::Request();
     request.set_id(3);
     std::unique_ptr<grpc::ClientReader<test::Response>> reader;
+while(true)
+{
+    grpc::ClientContext context;
+    //      std::chrono::system_clock::time_point timePoint(std::chrono::system_clock::now() + std::chrono::seconds(30));
+    //    context_->set_deadline(timePoint);
 
-        grpc::ClientContext context;
-        //      std::chrono::system_clock::time_point timePoint(std::chrono::system_clock::now() + std::chrono::seconds(30));
-        //    context_->set_deadline(timePoint);
+    assert(stub_);
+    reader = stub_->test(&context, request);
+    /*
+     * ToFix: We want to read forever and drop out only if stop_ is true. Meanwhile, if we dropped out of the reader,
+     * we have to check whether the context_ deadline has ended or the connection has broken. If the deadline has ended,
+     * reconnection is not needed, only reader->Read request should be provided.
+     * Suggested solution: wrap this while into
+     * while(!stop && CheckChannelReadiness(channel_))
+     */
 
-        assert(stub_);
-        reader = stub_->test(&context, request);
-        /*
-         * ToFix: We want to read forever and drop out only if stop_ is true. Meanwhile, if we dropped out of the reader,
-         * we have to check whether the context_ deadline has ended or the connection has broken. If the deadline has ended,
-         * reconnection is not needed, only reader->Read request should be provided.
-         * Suggested solution: wrap this while into
-         * while(!stop && CheckChannelReadiness(channel_))
-         */
-
-        while (reader->Read(&stream))
-        {
-            std::cout << "Received response, id: " << stream.id() << std::endl;
-        }
-
+    while (reader->Read(&stream))
+    {
+        std::cout << "Received response, id: " << stream.id() << std::endl;
+    }
+    auto status = reader->Finish();
+    std::cout<<status.error_message();
+}
 }
 
 HelloClient::HelloClient(std::shared_ptr<grpc::Channel> channel)
